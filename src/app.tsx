64ccdb9008631c4f82c8ad480728410c9d776d4f
Eryk Mol <moleryk7@gmail.com>
2018-08-08 15:28:12 +0200

    Create Schedule planner.
    Create Schedule planner by modifying pre-existing TODO application.

--- src/app.tsx
+++ src/app.tsx
@@ -1,122 +1,224 @@
-import { ui, CheckBox, TextView, TextInputAcceptEvent, Button, RadioButtonSelectEvent } from 'tabris';
-ui.drawer.enabled = true;
+import { ui, TextView, Page, NavigationView, TextInputAcceptEvent, Button, TextInput } from 'tabris';
 
-interface Task {
-  checked: boolean;
-  text: string;
+class Event {
+  public date: string;
+  public time: string;
+  public duration: string;
+  public description: string;
+  public text: string;
 }
 
-let tasksCompleted = 0;
-let allTasks = 0;
+const eventMap = new Map<Event,Button>();
 
-const taskCreate = (checkBoxEvent: any , checkedState= false) => {
-  const newTask = new CheckBox({
-    left: 29, top: 'prev() 30', right: 'next() 29',
-    checked: checkedState,
-    textColor: '#ff16a2',
-    text: checkBoxEvent.text
-  }).on({checkedChanged: ({ value }) => {
-      if(value === true) {
-        tasksDisplay.text = (++tasksCompleted) + ' tasks complete out of ' + allTasks;
-      } else {
-        tasksDisplay.text = (--tasksCompleted) + ' tasks complete out of ' + allTasks;
+const eventErrorMapCheck = (inputCount: number, event: Event, titleButton: Button) => {
+  if(eventMap.size!==0 && event.date!==undefined && event.time!==undefined && event.duration!==undefined){
+    const currentSplitDate = event.date.split('.');
+    const currentSplitTime = event.time.split(':');
+    for(const [savedEvent,savedTitleButton] of eventMap){
+      const savedSplitDate = savedEvent.date.split('.');
+      const savedSplitTime = savedEvent.time.split(':');
+      if(titleButton.text!==savedTitleButton.text && Number(savedSplitDate[0])===Number(currentSplitDate[0]) &&
+      Number(savedSplitDate[1])===Number(currentSplitDate[1]) &&
+      Number(savedSplitDate[2])===Number(currentSplitDate[2]) &&
+      (Number(savedSplitTime[0])===Number(currentSplitTime[0]) ||
+      Number(currentSplitTime[0])+Number(event.duration)>Number(savedSplitTime[0]))) {
+        throw new Error('conflicts with: ' + savedTitleButton.text);
       }
-      saving();
     }
-  }).appendTo(ui.contentView);
-  new Button({
-    right: 29, top: 'prev() -32',
-    text: 'Delete'
-  }).on({select: ({target}) => {
-      if(newTask.checked === true) {
-        tasksDisplay.text = (--tasksCompleted) + ' tasks complete out of ' + (--allTasks);
-      } else {
-        tasksDisplay.text = tasksCompleted + ' tasks complete out of ' + (--allTasks);
-      }
-      newTask.dispose(),
-      target.dispose(),
-      saving();
-    }
-  }).appendTo(ui.contentView);
+  }
+  if(inputCount===4) {
+    eventMap.set(event,titleButton);
+  }
+  saving();
 };
 
-const saveRestore = (option: number) => {
-  const tasks: object = JSON.parse(localStorage.getItem('tasksArray'));
-  if (!isCheckboxArray(tasks)) {
-    throw new Error('Unexpected format of checkbox array.');
+function edit(changedEvent: Event, titleName: string = 'Edit Page', titleButton: Button, eventPage: Page = new Page()) {
+  let inputCount = 0;
+  changedEvent.text = titleButton.text;
+  const textViewCount = eventPage.find(TextView);
+  const editPage = new Page({
+    title: titleName
+  }).appendTo(navigationView);
+  new TextInput({
+    top: 'prev() 30', left: 40, font: '16px', right: 40,
+    message: 'Date'
+  }).on({accept: ({ target }) => {
+      if (textViewCount.length < 4) {
+        changedEvent.date = target.text;
+        inputCount = inputCount + 1;
+        eventErrorMapCheck(inputCount,changedEvent,titleButton);
+      }
+      else {
+        textViewCount[0].text = target.text;
+        changedEvent.date = target.text;
+        inputCount = inputCount + 1;
+        eventErrorMapCheck(inputCount,changedEvent,titleButton);
+      }
+    }
+  }).appendTo(editPage);
+  new TextInput({
+    top: 'prev() 30', left: 40, font: '16px', right: 40,
+    message: 'Time'
+  }).on({
+    accept: ({ target }) => {
+      if (textViewCount.length < 4) {
+        changedEvent.time = 'Date: ' + target.text;
+        inputCount = inputCount + 1;
+        eventErrorMapCheck(inputCount,changedEvent,titleButton);
+      }
+      else {
+        textViewCount[1].text = 'Time: ' + target.text;
+        changedEvent.time = target.text;
+        inputCount = inputCount + 1;
+        eventErrorMapCheck(inputCount,changedEvent,titleButton);
+      }
+    }
+  }).appendTo(editPage);
+  new TextInput({
+    top: 'prev() 30', left: 40, font: '16px', right: 40,
+    message: 'Duration'
+  }).on({accept: ({ target }) => {
+      if (textViewCount.length < 4) {
+        changedEvent.duration = target.text;
+        inputCount = inputCount + 1;
+        eventErrorMapCheck(inputCount,changedEvent,titleButton);
+      }
+      else {
+        textViewCount[2].text = 'Duration: ' + target.text + 'h';
+        changedEvent.duration = target.text;
+        inputCount = inputCount + 1;
+        eventErrorMapCheck(inputCount,changedEvent,titleButton);
+      }
+    }
+  }).appendTo(editPage);
+  new TextInput({
+    top: 'prev() 30', left: 40, font: '16px', right: 40,
+    message: 'Description'
+  }).on({accept: ({ target }) => {
+      if (textViewCount.length < 4) {
+        changedEvent.description = target.text;
+        inputCount = inputCount + 1;
+        eventErrorMapCheck(inputCount,changedEvent,titleButton);
+      }
+      else {
+        textViewCount[3].text = 'Description: ' + target.text;
+        changedEvent.description = target.text;
+        inputCount = inputCount + 1;
+        eventErrorMapCheck(inputCount,changedEvent,titleButton);
+      }
+    }
+  }).appendTo(editPage);
+}
+
+const checkHighlight = (eventButtonMap: Map<Event,Button>) => {
+  const nowDate = new Date();
+  const nextEventDisplay = frontPage.find(TextView).first();
+  for(const [event,titleButton] of eventButtonMap){
+    const splitDate = event.date.split('.');
+    const splitTime = event.time.split(':');
+    if (Number(splitDate[0])===nowDate.getDate() &&
+      Number(splitDate[1])===(nowDate.getMonth() + 1) &&
+      Number(splitDate[2])===nowDate.getFullYear()) {
+        if ((nowDate.getHours()===Number(splitTime[0]) && nowDate.getMinutes()>=Number(splitTime[1])) ||
+        (nowDate.getHours()>Number(splitTime[0]) && nowDate.getHours()-Number(splitTime[0])<Number(event.duration))) {
+        titleButton.textColor = '#f202ff';
+      }else if(nowDate.getHours()<Number(splitTime[0])){
+        nextEventDisplay.text = 'Next coming event: ' + titleButton.text;
+      }else {
+        titleButton.dispose();
+      }
+    }else if(Number(splitDate[0])<nowDate.getDate() ||
+      Number(splitDate[1])<(nowDate.getMonth() + 1) ||
+      Number(splitDate[2])<nowDate.getFullYear()) {
+        titleButton.dispose();
+    }else if(Number(splitDate[0])-nowDate.getDate()<=4 && Number(splitDate[0])-nowDate.getDate()>0 &&
+      Number(splitDate[1])===(nowDate.getMonth() + 1) &&
+      Number(splitDate[2])===nowDate.getFullYear()){
+        nextEventDisplay.text = 'Next coming event: ' + titleButton.text;
+    }
   }
-  const savedTasksCompleted = Number(localStorage.getItem('tasksCompleted'));
-  const savedAllTasks = Number(localStorage.getItem('allTasks'));
-  tasksCompleted = savedTasksCompleted;
-  allTasks = savedAllTasks;
-  tasksDisplay.text = tasksCompleted + ' tasks complete out of ' + allTasks;
-  for (const task of tasks) {
-    if(task.checked === false && option === 1) {taskCreate(task,task.checked);}
-    if(task.checked === true && option === 2) {taskCreate(task,task.checked);}
-    if(option === 3) {taskCreate(task,task.checked);}
+};
+
+const eventCreate = (buttonEvent: any, newEvent= new Event(), restoreSaveFlag= false) => {
+  const eventTitle = new Button({
+    top: 'prev() 30', left: 40,
+    text: buttonEvent.text
+  }).on({select: () => {
+      const displayPage = new Page({
+        title: 'Event Page'
+      }).appendTo(navigationView);
+      new TextView({
+        top: 'prev() 30', left: 40, font: 'bold 16px',
+        text: 'Date: ' + newEvent.date
+      }).appendTo(displayPage);
+      new TextView({
+        top: 'prev() 30', left: 40, font: 'bold 16px',
+        text: 'Time: ' + newEvent.time
+      }).appendTo(displayPage);
+      new TextView({
+        top: 'prev() 30', left: 40, font: 'bold 16px',
+        text: 'Duration: ' + newEvent.duration + 'h'
+      }).appendTo(displayPage);
+      new TextView({
+        top: 'prev() 30', left: 40, font: 'bold 16px', right: 40,
+        text: 'Description: ' + newEvent.description
+      }).appendTo(displayPage);
+      new Button({
+        top: 'prev() 30', left: 40,
+        text: 'Edit'
+      }).on({select: () => {
+          edit(newEvent,'Edit Page',eventTitle,displayPage);
+        }
+      }).appendTo(displayPage);
+    }
+  }).appendTo(frontPage);
+  if(restoreSaveFlag===false) {
+    edit(newEvent, 'Event Page', eventTitle); }else { eventMap.set(newEvent,eventTitle); }
+};
+
+function isEventArray(value: any): value is Event[] {
+  return value instanceof Array
+    && value.every((entry: Event) => typeof entry.date === 'string' && typeof entry.time === 'string' &&
+    typeof entry.duration === 'string' && typeof entry.description === 'string');
+}
+
+const saveRestore = () => {
+  const events: object = JSON.parse(localStorage.getItem('eventsArray'));
+  if (!isEventArray(events)) {
+    throw new Error('Unexpected format of event array.');
+  }
+  for (const event of events) {
+    eventCreate(event,event,true);
   }
 };
 
 const saving = () => {
   localStorage.clear();
-  const countBox = ui.find(CheckBox);
-  const tasksArray: Task[] = [];
-  countBox.forEach((element) => {
-    const taskData = {
-      checked: element.checked,
-      text: element.text
-    };
-    tasksArray.push(taskData);
+  const eventsArray: Event[] = [];
+  eventMap.forEach((value,key) => {
+    eventsArray.push(key);
   });
-  localStorage.setItem('tasksArray', JSON.stringify(tasksArray));
-  localStorage.setItem('tasksCompleted', JSON.stringify(tasksCompleted));
-  localStorage.setItem('allTasks', JSON.stringify(allTasks));
+  localStorage.setItem('eventsArray', JSON.stringify(eventsArray));
 };
 
 const tempName = (event: TextInputAcceptEvent) => {
-  if(event.text.length>21) {
-    throw new Error('Task description too long.');
+  if (event.text.length > 21) {
+    throw new Error('Event title too long.');
   }
-  tasksDisplay.text = tasksCompleted + ' tasks complete out of ' + (++allTasks);
-  taskCreate(event);
-  saving();
+  eventCreate(event);
 };
 
-const options = (event: RadioButtonSelectEvent) => {
-  if (event.target.text === 'All tasks' && event.target.checked === true) {
-    ui.find(CheckBox).dispose();
-    ui.find(Button).dispose();
-    saveRestore(3);
-  }
-  if (event.target.text === 'Finished tasks' && event.target.checked === true) {
-      ui.find(CheckBox).dispose();
-      ui.find(Button).dispose();
-      saveRestore(2);
-  }
-  if (event.target.text === 'Unfinished tasks' && event.target.checked === true) {
-      ui.find(CheckBox).dispose();
-      ui.find(Button).dispose();
-      saveRestore(1);
-  }
-};
+const navigationView = new NavigationView({
+  left: 0, top: 0, right: 0, bottom: 0
+}).appendTo(ui.contentView);
 
-ui.contentView.append(
-  <textView centerX={0} id='input' text='0 tasks complete out of 0' top={12} font='20px' />
+const frontPage = new Page({
+  title: 'Event list'
+}).appendTo(navigationView);
+
+frontPage.append(
+  <textInput top='prev() 20' message='Type the name of an event' onAccept={tempName} centerX={0}/>,
+  <textView top='prev() 20' id='input' text='No upcoming events' font='20px' centerX={0}/>
 );
-
-const tasksDisplay = ui.find(TextView).first();
-
-ui.contentView.append(
-  <textInput top='prev() 20' left='20%' right='20%' message='Type here, then confirm' onAccept={tempName} />
-);
-
-ui.drawer.append(
-  <radioButton left={30}  top='prev() 25' text='All tasks' checked={false} onSelect={options}/>,
-  <radioButton left={30}  top='prev() 25' text='Finished tasks' checked={false} onSelect={options}/>,
-  <radioButton left={30}  top='prev() 25' text='Unfinished tasks' checked={false} onSelect={options}/>
-);
-
-function isCheckboxArray(value: any): value is Task[] {
-  return value instanceof Array
-    && value.every((entry: Task) => typeof entry.checked === 'boolean' && typeof entry.text === 'string');
-}
+saveRestore();
+setInterval(checkHighlight,3000,eventMap);
